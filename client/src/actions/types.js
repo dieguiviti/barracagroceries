fccdecf163b94524f075674b2aefc01b0aa7488b
dieguiviti <dieguiviti@gmail.com>
2020-02-28 12:14:03 +0100

    Added users' routes' action types for redux-react interaction

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,17 @@
-export const GET_ITEMS = 'GET_ITEMS';
-export const ADD_ITEM = 'ADD_ITEM';
-export const DELETE_ITEM = 'DELETE_ITEM';
-export const ITEMS_LOADING = 'ITEMS_LOADING';
+// Items' routes' action types
+export const ITEMS_LOADING = 'ITEMS_LOADING';       // User is loading items
+export const GET_ITEMS = 'GET_ITEMS';               // User got the items
+export const ADD_ITEM = 'ADD_ITEM';                 // User added an item
+export const DELETE_ITEM = 'DELETE_ITEM';           // User deleted an item
+
+// Users' routes' action types
+export const USER_LOADING = 'USER_LOADING';         // User is loading
+export const USER_LOADED = 'USER_LOADED';           // User loaded
+export const AUTH_ERROR = 'AUTH_ERROR';             // User Authentication had error
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';       // User login was a success
+export const LOGIN_FAIL = 'LOGIN_FAIL';             // User login failed
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';     // User logout was a success
+export const REGISTER_SUCCESS = 'REGISTER SUCCESS'; // User registered successfully
+export const REGISTER_FAIL = 'REGISTER_FAIL';       // User registration failed
+export const GET_ERRORS = 'GET_ERRORS';             // Get authentication errors
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';         // Clear auth errors 
